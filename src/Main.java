347f254fb9c3c75e06986b765e866ccefceb269c
nazmigorkem <nazmigorkem@hotmail.com>
2023-04-14 22:22:42 +0300

    updated readme.md

--- src/Main.java
+++ src/Main.java
@@ -22,7 +22,6 @@ public class Main {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
-        System.out.println(program);
         for (Token token : new Parser().parse(program)) {
             System.out.println(token);
         }
